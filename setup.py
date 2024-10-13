6402f3ffb5a519e4ac2f15b47d18e33b481c86de
ARYAN SINGH <thisisaryan1ia@gmail.com>
2024-10-14 01:06:52 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aryan Singh',
+    author_email= 'codeearyane@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
